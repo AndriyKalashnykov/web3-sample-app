2bf36b163999a56971100fc9713a920172e9892f
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-09-14 09:35:57 -0400

    fix: updates
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/styles/main.css'
 import App from './App'
-import { store } from './store'
-import { Provider } from 'react-redux'
-import { ThemeProvider } from '@mui/material/styles'
-import { theme } from './theme'
+import {store} from './store'
+import {Provider} from 'react-redux'
+import {ThemeProvider} from '@mui/material/styles'
+import {theme} from './theme'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "@/dev";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <DevSupport ComponentPreviews={ComponentPreviews}
+                            useInitialHook={useInitial}
+                >
+                    <App/>
+                </DevSupport>
+            </Provider>
+        </ThemeProvider>
+    </React.StrictMode>,
 )
